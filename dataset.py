4a294aca29354be9f83fef5737d5810d0a2b53ae
Shayan Hashemi <shshemi@gmail.com>
2023-01-14 17:41:41 +0200

    add match index row to dataset

--- dataset.py
+++ dataset.py
@@ -1,26 +1,27 @@
 import csv
-import string
 from pathlib import Path
 
-from helpers import extract_keywords
+import numpy as np
 
 
 class Dataset:
-    def __init__(self, path, name=None, encoding='utf-8'):
+    def __init__(self, path, name=None, subsample=None, encoding='utf-8'):
         self.__x = []
         self.__y = []
         self.__c = []
-        cluster_table = {}
         with open(path, encoding=encoding) as f:
             reader = csv.reader(f)
             for row in reader:
                 assert len(row[0].strip()) == len(row[1].strip())
                 self.__x.append(row[0].strip())
                 self.__y.append(row[1].strip())
-                cluster_str = extract_keywords(row[0], row[1])
-                if cluster_str not in cluster_table:
-                    cluster_table[cluster_str] = len(cluster_table)
-                self.__c.append(cluster_table[cluster_str])
+                self.__c.append(row[2].strip())
+            if subsample is not None:
+                assert type(subsample) is int
+                indices = np.random.choice(np.arange(len(self.__x)), subsample)
+                self.__x = [self.__x[idx] for idx in indices]
+                self.__y = [self.__y[idx] for idx in indices]
+                self.__c = [self.__c[idx] for idx in indices]
         self.__name = name if name is not None else Path(path).name
 
     def __len__(self):

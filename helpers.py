b7ea941ad5ebb7934c960ed7e8886d66b1a37aa6
Shayan Hashemi <shshemi@gmail.com>
2023-01-14 17:42:30 +0200

    add fix whitespace function(used by parameter mask metrics)

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import re
 import string
 from typing import Sequence
 
@@ -11,3 +12,15 @@ def extract_keywords(message: str, mask: str, junk=None):
     if junk is None:
         junk = frozenset(string.whitespace + string.punctuation)
     return frozenset(''.join(cx if cy == '0' and cx not in junk else ' ' for cx, cy in zip(message, mask)).split())
+
+
+def fix_whitespace_problem(x, mask):
+    new_mask = []
+    bidx = 0
+    for m in re.finditer(r'\s{2,}', x):
+        new_mask.append(mask[bidx: m.start()])
+        new_mask.append('0' * (m.end() - m.start()))
+        bidx = m.start() + 1
+        # mask = mask[:m.start()] + ('0' * (m.end() - m.start())) + mask[m.start() + 1:]
+    new_mask.append(mask[bidx:])
+    return ''.join(new_mask)

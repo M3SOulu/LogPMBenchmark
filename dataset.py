252d1d1470d0122b862352dd642e913eb3701e8d
Shayan Hashemi <shshemi@gmail.com>
2023-03-16 18:33:46 +0200

    add automatic downloading for datasets

--- dataset.py
+++ dataset.py
@@ -1,14 +1,25 @@
 import csv
+import multiprocessing
+import os.path
 from pathlib import Path
 
 import numpy as np
+import requests
+from tqdm import tqdm
+
+DATASET_NAMES = ('hpc', 'zookeeper', 'android', 'apache', 'hadoop', 'hdfs', 'linux', 'openstack', 'proxifier', 'ssh')
+download_lock = multiprocessing.Lock()
 
 
 class Dataset:
-    def __init__(self, path, name=None, subsample=None, encoding='utf-8'):
+    def __init__(self, name, subsample=None, encoding='utf-8'):
         self.__x = []
         self.__y = []
         self.__c = []
+        assert name in DATASET_NAMES, f"Invalid dataset name: {name}, it should be chosen from {DATASET_NAMES}"
+        path = f'data/{name}.csv'
+        if not os.path.exists(path):
+            download_dataset(name)
         with open(path, encoding=encoding) as f:
             reader = csv.reader(f)
             for row in reader:
@@ -41,3 +52,40 @@ class Dataset:
     @property
     def c(self):
         return self.__c
+
+
+def check_data_directory():
+    data_dir = Path('data')
+    if not data_dir.exists():
+        data_dir.mkdir()
+    elif not data_dir.is_dir():
+        print("data should be a directory but it is not. Consider removing it and running the benchmark again.")
+        exit(1)
+
+
+def download_dataset(name: str, block_size=2048):
+    check_data_directory()
+    url = f'https://zenodo.org/record/7741390/files/{name}.csv'
+    temp_path = f'data/.{name}.csv'
+    file_path = f'data/{name}.csv'
+    if os.path.exists(file_path):
+        return
+    print(f"Dataset '{name}' were not found in '{file_path}' so downloading it from '{url}'")
+    with download_lock:
+        response = requests.get(url, stream=True)
+        if not response:
+            print(f"Unable to download {name}")
+            exit(1)
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        with open(temp_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
+            exit(1)
+        else:
+            os.rename(temp_path, file_path)
+            print(f"{name} downloaded successfully")
